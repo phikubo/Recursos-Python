6809075889ad6df1252bc09bced86a1bcad5c3dd
Michael Mart <jmichaelten@gmail.com>
2018-11-26 16:59:45 -0500

    Basis nearest neightbors

---
+++ caso3/vecinos_cercanos.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__=="__main__":
+	puntos=np.array([ [1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3] ])
+	print(puntos)
+	print("longitud array: ",len(puntos))
+	print("Primera posicion: ", puntos[0], "segunda posicion: ", puntos[1], "En la segunda posicion, adentro el segundo valor: ",puntos[1][1])
+	print(puntos[:3]) #Hasta la n posicion: array[:n] ->1,2,3,...,n
+	print(puntos[:,1]) #vector columna posicion k: array[:,k]
+	p= np.array([2.5,2])
+	#plt.plot(puntos[:,0], puntos[:,1], "ro")
+	#plt.show()
+else:
+	print("modulo importado","Caso 3: KNN")
+
