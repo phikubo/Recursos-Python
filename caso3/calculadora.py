fab665d71b416799e031db37cbf0d484f48d4872
Michael Mart <jmichaelten@gmail.com>
2018-11-26 16:45:59 -0500

    Lo ultimo

--- caso3/calculadora.py
+++ caso3/calculadora.py
@@ -23,7 +23,13 @@ def contar_votos(votos):
 			print(mayores, "obtuvo la mayor cantidad de votos")
 	return votos_cuenta, mayores, random.choice(mayores)
 	
-
+import scipy.stats as ss
+def contar_votos_scipy(votos):
+	"""
+	Cuenta el numero de veces que esta el valor de un array. Funciona cuando hay empates (regresa ambos)
+	"""
+	moda, cuenta= ss.mstats.mode(votos)
+	return moda,cuenta
 if __name__=="__main__":
 	votos=[1,2,3,4,5,1,2,3,1,2,6,5,7,12,15,1,1,1,1,1,7,7,7,7,7,7,7]
 	test,test2,test3=contar_votos(votos)
@@ -32,6 +38,7 @@ if __name__=="__main__":
 	print("maximo de numeros",max(test.keys()))
 	print("maximo de votos",max(test.values()))
 	print("mayor ", test2, "eleccion de uno: ",test3)
+	print("usando scipy ",contar_votos_scipy(votos))
 else:
 	print("modulo importado","Caso 3: calculadora")
 
